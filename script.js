869af658fd671398deabd5bd62deae8bb8c2bb5d
Breno Garcia <BrenoGHeat@gmail.com>
2024-01-09 17:57:35 -0300

    script.js resolved

--- script.js
+++ script.js
@@ -10,3 +10,60 @@ const tasks = [
   {title: "Pagar a conta de energia", type: "Urgente"},
   {title: "Assistir a um documentário interessante", type: "Normal"},
 ];
+
+
+function renderElements(tasks){
+  
+  const captureUl = document.querySelector(".tasks__list");
+
+    captureUl.innerHTML = ""
+
+      for(let i = 0; i < tasks.length ; i++) {
+        const task = createTaskItem(tasks[i]);
+          captureUl.appendChild(task);
+      
+ }
+} ;
+
+
+function createTaskItem(task){
+
+  const li = document.createElement("li");
+        li.classList.add("task__item");
+
+
+  const div = document.createElement("div");
+         div.classList.add("task-info__container");
+
+  const span = document.createElement("span");
+        span.classList.add("task-type");
+
+
+  const paragraphSpan = document.createElement("p");
+          paragraphSpan.innerText = task.title ;
+
+
+  const liButton = document.createElement("button");
+        liButton.classList.add("task__button--remove-task");
+
+
+        if( task.type === "Urgente" ){
+          span.classList.add("span-urgent");
+        } else if ( task.type === "Importante"){
+          span.classList.add("span-important"); 
+        } else if( task.type === "Normal") {
+          span.classList.add("span-normal");
+        } ;
+
+        div.appendChild(span);
+        div.appendChild(paragraphSpan);
+
+        li.appendChild(div);
+        li.appendChild(liButton);
+
+        return li ;
+
+}
+
+renderElements(tasks);
+
